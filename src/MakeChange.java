c71751561fb8707a52df58f0fea3e8a8d81324dd
Emily Lau <emilylau@Brucebs-MacBook-Pro-2.local>
2019-05-10 17:47:16 -0600

    Initial Commit

---
+++ src/MakeChange.java
@@ -0,0 +1,14 @@
+
+public class MakeChange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//prompt user for value, and get input
+		
+//		prompt user for amount tendered, and get input
+		
+		
+	}
+
+}
